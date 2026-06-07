01a0f8992b0e52887a2d579134a4f69b79050e84
Anubhav Mishra <25114cn108glbitm@gmail.com>
2026-06-07 16:48:57 +0530

    day 1

---
+++ c1.c
@@ -0,0 +1,17 @@
+//program to calculate sum of first N natural numbers
+
+#include <stdio.h>
+
+int main() {
+    int N;
+    int sum;
+
+    printf("Enter a number: ");
+    scanf("%d", &N);
+
+    sum = N * (N + 1) / 2;
+
+    printf("Sum of first %d natural numbers = %d\n", N, sum);
+
+    return 0;
+}
