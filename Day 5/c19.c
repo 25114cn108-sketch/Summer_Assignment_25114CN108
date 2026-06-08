79f9dadee6dd70bc022afa330a7beffd594ede03
Anubhav Mishra <25114cn108glbitm@gmail.com>
2026-06-08 12:39:59 +0530

    Day 5

---
+++ Day 5/c19.c
@@ -0,0 +1,17 @@
+//program to Print factors of a number. 
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    printf("Factors: ");
+
+    for(int i = 1; i <= n; i++) {
+        if(n % i == 0)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
