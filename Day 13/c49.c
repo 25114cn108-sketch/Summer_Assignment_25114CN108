0c83a7ec93bb3e65c62d8554d8363616544f4d29
Anubhav Mishra <25114cn108glbitm@gmail.com>
2026-06-13 18:22:03 +0530

    Day 13

---
+++ Day 13/c49.c
@@ -0,0 +1,18 @@
+//a program to Input and display array.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
---
+++ Day 13/c51.c
@@ -0,0 +1,28 @@
+//a program to Find largest and smallest element.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int largest = arr[0], smallest = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > largest)
+            largest = arr[i];
+
+        if(arr[i] < smallest)
+            smallest = arr[i];
+    }
+
+    printf("Largest = %d\n", largest);
+    printf("Smallest = %d", smallest);
+
+    return 0;
+}
